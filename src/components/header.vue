82b6ee5af7d654dbaf6aed08bfa3ae9bfe76f6ab
Sosokantaria <121603634+Sosokantaria@users.noreply.github.com>
2024-07-06 02:39:58 +0400

    fixed herader name

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="flex justify-between h-[42px] mx-3 md:mx-0 mt-2 md:mt-5 ">
-    <div class="flex items-center  md:mt-5 ">
-      <div class="text-teal-lite hidden laptop:flex  ">
+  <div class="flex justify-between h-[42px] mx-3 md:mx-0 mt-2 md:mt-5">
+    <div class="flex items-center md:mt-5">
+      <div class="text-teal-lite hidden laptop:flex">
         <img src="../assets/icons/chevron-right.svg" class="colot-teal" />
       </div>
       <span
+        v-if="$route.name !== 'Offers'"
         class="hidden font-robotoSlab text-black laptop:flex text-5xl font-[700] leading-[39.57px]"
         >{{ $route.name }}</span
+      ><span
+        v-else-if="$route.name === 'Offers'"
+        class="hidden font-robotoSlab text-black laptop:flex text-5xl font-[700] leading-[39.57px]"
+        >Offers from Specialists</span
       >
       <span class="text-black text-3xl font-[700] flex md:hidden"
         >Hi, Andrea</span
       >
     </div>
     <div class="flex justify-end items-center h-[42px]">
-      <div class=" flex items-center  mt-1.5 mr-7 md:mr-4 relative">
-        <img
-          src="../assets/icons/bell-outline.svg"
-          class=" h-[19px] w-[19px]"
-        />
+      <div class="flex items-center mt-1.5 mr-7 md:mr-4 relative">
+        <img src="../assets/icons/bell-outline.svg" class="h-[19px] w-[19px]" />
         <div
           v-if="activities > 0"
           class="flex justify-center text-[10px] text-white font-[700] items-center rounded-[19px] w-[28px] h-[18px] absolute top-[-5px] left-[9px] bg-red"
         >
-        <span class="font-[700] text-sm1 text-white leading-[14px]">{{ activities }}</span>  
+          <span class="font-[700] text-sm1 text-white leading-[14px]">{{
+            activities
+          }}</span>
         </div>
       </div>
-      <hr class="hidden md:flex w-[2px] h-[28px] mx-3 bg-gray-lite"/>
+      <hr class="hidden md:flex w-[2px] h-[28px] mx-3 bg-gray-lite" />
       <div class="flex items-centre gap-3">
         <span
           class="text-black text-3xl font-[700] leading-[23.44px] md:flex items-center hidden"
@@ -37,28 +41,37 @@
           <img
             src="../assets/icons/account-circle.svg"
             class="h-[24px] w-[24px]"
-          /><span class="text-sm font-[700] leading-[14.06px] text-teal-lite">FARMER</span>
+          /><span class="text-sm font-[700] leading-[14.06px] text-teal-lite"
+            >FARMER</span
+          >
         </router-link>
       </div>
-      <hr class="hidden md:flex w-[2px] h-[28px] mx-3 bg-gray-lite"/>
-      <div >
+      <hr class="hidden md:flex w-[2px] h-[28px] mx-3 bg-gray-lite" />
+      <div>
         <router-link
           :to="{ name: 'FindSpecialists' }"
-          class=" bg-teal-lite  hidden tablet:flex justify-center gap-3 items-center rounded-[8px] shadow-lg h-[42px] w-[191px]  "
+          class="bg-teal-lite hidden tablet:flex justify-center gap-3 items-center rounded-[8px] shadow-lg h-[42px] w-[191px]"
         >
           <span class="text-xl font-[700] text-white">Find Specialists</span
-          ><img src="../assets/icons/account-search-outline 1.svg" class="h-[24px] w-[24px]" />
+          ><img
+            src="../assets/icons/account-search-outline 1.svg"
+            class="h-[24px] w-[24px]"
+          />
         </router-link>
       </div>
     </div>
   </div>
-  <div
-    class="laptop:hidden text-black mt-5  flex h-[40px] items-center  "
-  >
-    <img src="../assets/icons/chevron-right.svg"  />
-    <span class="text-5xl font-[700] leading-[39.57px] font-robotoSlab">{{
-      $route.name
-    }}</span>
+  <div class="laptop:hidden text-black mt-5 flex h-[40px] items-center">
+    <img src="../assets/icons/chevron-right.svg" />
+    <span
+      v-if="$route.name !== 'Offers'"
+      class="hidden font-robotoSlab text-black laptop:flex text-5xl font-[700] leading-[39.57px]"
+      >{{ $route.name }}</span
+    ><span
+      v-else-if="$route.name === 'Offers'"
+      class="hidden font-robotoSlab text-black laptop:flex text-5xl font-[700] leading-[39.57px]"
+      >Offers from Specialists</span
+    >
   </div>
 </template>
 
